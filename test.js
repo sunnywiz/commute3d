55adbf4ba37d5d2b53816c87c05b99cf2ae40725
Sunny Gulati <sunnywiz@gmail.com>
2017-04-27 18:09:14 -0400

    Got "reading CSV file and getting lat/long/time" figured out.

--- test.js
+++ test.js
@@ -1,5 +1,23 @@
-var cad = require ('jscad'); 
+// var cad = require ('jscad'); 
+// var c = cube({size:[2,4,96]});
+// cad.renderFile(c, 'output.stl'); 
 
-var c = cube({size:[2,4,96]});
+const fs = require('fs'); 
+var csvdata = require("csvdata");
 
-cad.renderFile(c, 'output.stl'); 
+var clat = " Latitude"; 
+var clon = " Longitude"; 
+var ctime = " Device Time";
+var p = csvdata.load("input/trackLog-2012-Dec-30_18-26-55.csv");
+
+p.then(function(x) { 
+  x.forEach(function(y)  { 
+    if (y[clat] && y[clon] && y[ctime]) { 
+      var lat = y[clat]; 
+      var long = y[clon]; 
+      var time = Date.parse(y[ctime]);
+      console.log(time);  
+    }
+  });
+}); 
+  
